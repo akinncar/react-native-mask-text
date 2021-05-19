1d55ad5a17a24ace38f2f1132b09d450a3022023
Akinn Rosa <akinncar@hotmail.com>
2021-05-19 19:05:53 -0300

    Add first example off masked input

--- example/App.js
+++ example/App.js
@@ -1,12 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { MaskedTextInput } from "react-native-mask-text";
 
 export default function App() {
+  const [maskedValue, setMaskedValue] = useState("");
+  const [unMaskedValue, setUnmaskedValue] = useState("");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MaskedTextInput
+        mask="99/99/9999"
+        onChangeText={(text, rawText) => {
+          setMaskedValue(text);
+          setUnmaskedValue(rawText);
+        }}
+        style={styles.input}
+      />
+      <Text style={styles.paragraph}>Raw Text: {unMaskedValue}</Text>
+      <Text style={styles.paragraph}>Masked Text: {maskedValue}</Text>
     </View>
   );
 }
@@ -14,8 +25,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    textAlign: "center",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
   },
 });
