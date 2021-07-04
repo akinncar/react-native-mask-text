d1b7e075b66fa2fa98895d96c894255bfa74dac9
João Augusto Perin de Souza <joaoaugustoperin@gmail.com>
2021-07-04 15:49:01 -0300

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+      browser: true,
+      es2021: true,
+      node: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+      ecmaFeatures: {
+          jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: 'module',
+  },
+  plugins: ['prettier', 'react', '@typescript-eslint'],
+  rules: {
+      'prettier/prettier': 'error',
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+      'func-names': 'off',
+      'no-process-exit': 'off',
+      'object-shorthand': 'off',
+      'class-methods-use-this': 'off',
+  },
+}
