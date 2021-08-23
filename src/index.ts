ac739538e44b777a06fabc59ce4f623291b448a9
Jimmi Jønsson <jsh@usekeyhole.com>
2021-08-23 10:58:31 +0200

    Exporting MaskedTextProps & MaskedTextInputProps types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import { MaskedText } from './components/MaskedText'
-import { MaskedTextInput } from './components/MaskedTextInput'
+import { MaskedText, MaskedTextProps } from './components/MaskedText'
+import { MaskedTextInput, MaskedTextInputProps } from './components/MaskedTextInput'
 
 import { mask, unMask } from './utils/mask'
 
 export { MaskedText, MaskedTextInput, mask, unMask }
+
+export type { MaskedTextProps, MaskedTextInputProps }
